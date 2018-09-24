55e1315734a027f6a9e3e7787d631a04bb30b27d
Joshua Goodman <z5117211@student.unsw.edu.au>
2018-09-24 13:28:15 +1000

    simple version working

--- receiver.py
+++ receiver.py
@@ -12,138 +12,159 @@ from socket import *
 
 
 
-    class STPPacket:
+class Packet:
 
-	    def __init__(self, data, seqNum, ackNum, ack = False, syn = False, fin = False):
+    def __init__(self, data, seqNum, ackNum, ack = False, syn = False, fin = False):
 
-	        self.data = data
-	        self.seqNum = seqNum
-	        self.ackNum = ackNum
-	        self.ack = ack
-            self.syn = syn
-	        self.fin = fin
-	    
+        self.data = data
+        self.seqNum = seqNum
+        self.ackNum = ackNum
+        self.ack = ack
+        self.syn = syn
+        self.fin = fin
+    
 
-    class receiver:
+class Receiver:
 
-        def __init__(self, recPort, fileCopy):
-            self.recPort = recPort
-            self.fileCopy = fileCopy
-            
-            
+    def __init__(self, recPort, fileCopy):
+        self.recPort = int(recPort)
+        self.fileCopy = fileCopy
         
-        socket = socket(AF_INET, SOCK_DGRAM)
         
-        def receivePacket(self):
-            data, addr = self.socket.recvfrom(2048)
-            packet = pickle.loads(data)
-            return packet, addr
-            
+    
+    socket = socket(AF_INET, SOCK_DGRAM)
+    
+    def receivePacket(self):
+        data, addr = self.socket.recvfrom(2048)
+        packet = pickle.loads(data)
+        return packet, addr
         
-        def appendData(self, data):
-            f = open(fileCopy, "a+")
-            f.write(data)
-            f.close()
+    
+    def appendData(self, data):
+        f = open(fileCopy, "a+")
+        f.write(data)
+        f.close()
 
+    
+    def sendPacket(self, packet, address):
+        self.socket.sendto(pickle.dumps(packet), address)
         
-        def sendPacket(self, packet, address):
-            self.socket.sendto(pickle.dumps(packet), address)
-            
+    
+    #def updateLog(self , , ,)
+    
+    
+    
+    
+###main
+
+if (len(sys.argv) != 3):
+    print("incorect usage, needs two arguments")
+    exit(0)
+
+
+
+seqNum = 0
+ackNum = 0
+#sendbase = 0
+#numUnAck = 0
+
+# different states of operation
+noConnection = True
+synSent = False
+connected = False
+fin = False
+connectionFinished = False
+
+dataProgress = 0
+transCount = 0
+untranCount = 0
+dropCount = 0 
+expectedSeq = 0
+expectedAck = 0
+
+recPort, fileCopy = sys.argv[1:]
+receiver = Receiver(recPort, fileCopy)
+receiver.socket.bind(('', receiver.recPort))
+
+
+###main event
+
+while (connectionFinished == False):
+    
+    if (noConnection == True):
+        synPacket, address = receiver.receivePacket()
         
-        #def updateLog(self , , ,)
+        if (synPacket.syn == True):
+            ackNum = synPacket.seqNum + 1
+            synAckPacket = Packet('', seqNum, ackNum, ack = True, syn = True, fin = False)
+            receiver.sendPacket(synAckPacket, address)
+            noConnection = False
+            synSent = True
+            expectedAck = seqNum + 1
+    
+    if (synSent == True):
         
+        ackPacket, address = receiver.receivePacket()
         
+        if (ackPacket.ack == True and expectedAck == ackPacket.ackNum):
+            expectedSeq = ackPacket.seqNum
+            synSent = False
+            connected = True
+    
+    if (connected == True):
         
-        
-    ###main
-    
-    if (len(sys.argv) != 3):
-        print("incorect usage, needs two arguments")
-        return
-    
-    
-   
-    seqNum = 0
-    ackNum = 0
-    #sendbase = 0
-    #numUnAck = 0
-    
-    # different states of operation
-    noConnection = True
-    synSent = False
-    connected = False
-    finSent = False
-    connectionFinished = False
-    
-    transCount = 0
-    untranCount = 0
-    dropCount = 0 
-    expectedSeq = 0
-    expectedAck = 0
-    
-    recPort, fileCopy = sys.argv[1:]
-    receiver = Receiver(recPort, fileCopy)
-    receiver.socket.bind(('', receiver.recPort))
-    
-    
-    ###main event
-    
-    while (connectionFinished == False):
-        
-        if (noConnection == True):
-            synPacket, address = receiver.receivePacket
-            
-            if (synPacket.syn == True):
-                ackNum = synPacket.seqNum + 1
-                synAckPacket = Packet('', seqNum, ackNum, ack = True, syn = True, fin = False)
-                receiver.sendPacket(senAckPacket, address)
-                noConnection = False
-                synSent = True
+        while (connected == True):
+            print("waiting for packets")
+            packet, address = receiver.receivePacket() 
+           
+            if (packet.fin == False and packet.seqNum == expectedSeq):
+                receiver.appendData(packet.data)
+                dataProgress += len(packet.data)
+                seqNum = seqNum + 1
+                ackNum += len(packet.data)
+                expectedSeq = ackNum
                 expectedAck = seqNum + 1
-        
-        if (synSent = True):
-            
-            ackPacket, address = receiver.receivePacket()
-            
-            if (ackPacket.ack == True and expectedAck == ackPacket.ackNum):
-                expectedSeq = ackPacket.seqNum
-                synSent = False
-                connected = True
-        
-        if (connected == True):
-            
-            while True:
-            
-                packet, address = receiver.receivePacket() 
-               
-                if (packet.fin == False and packet.seqNum == expectedSeq):
-                    receiver.appendData(packet.data)
-                    dataProgress += len(packet.data)
-                    seqNum++
-                    ackNum += len(packet.data)
-                    expectedSeq = ackNum
-                    expectedAck = seqNum + 1
-                    ackPacket = Packet('', seqNum, ackNum, ack = True, syn = False, fin = False)
-                    receiver.sendPacket(ackPacket, address)
-                       
+                print("sending ack")
+                ackPacket = Packet('', seqNum, ackNum, ack = True, syn = False, fin = False)
+                receiver.sendPacket(ackPacket, address)
+                   
+
+            elif (packet.fin == True):
+                print("fin received")
+                #finish
+                connected = False
+                fin = True
+                seqNum = seqNum + 1
+                ackNum = packet.seqNum + 1
+                ackPacket = Packet('', seqNum, ackNum, ack = True, syn = False, fin = False)
+                receiver.sendPacket(ackPacket, address)
+                print("ack sent")
+            else:
+                print("packet out of order")
+                #packet out of order and add to buffer
     
-                else if (packet.fin == True):
-                    
-                    #finish
-                    connected = False
-                    finSent = True
-                else:
-                    
-                    #packet out of order and add to buffer
+    
+    
+    if (fin == True):
+        ###more finishing logic
+        finPacket = Packet('', seqNum, ackNum, ack = False, syn = False, fin = True)
+        receiver.sendPacket(finPacket, address)
+        print("fin sent")
+        
+        ##wait for ack for fin
         
         
-        if (finSent == True):
-            ###more finishing logic
-    
-    
-    
-    
- 
+        packet, address = receiver.receivePacket()
+        if packet.ack == True:
+            print("fin acknowledged")
+            fin = False
+            connectionFinished = True
+            receiver.socket.close()
+            print("connection closed, file received succesfully")
+            
+
+
+
     
     
     
--- sender.py
+++ sender.py
@@ -12,195 +12,203 @@ from socket import *
 
 
 
-    class Packet:
+class Packet:
 
-	    def __init__(self, data, seqNum, ackNum, ack=False, syn=False, fin=False):
+    def __init__(self, data, seqNum, ackNum, ack=False, syn=False, fin=False):
 
-	        self.data = data
-	        self.seq_num = seq_num
-	        self.ack_num = ack_num
-	        self.ack = ack
-            self.syn = syn
-	        self.fin = fin
+        self.data = data
+        self.seqNum = seqNum
+        self.ackNum = ackNum
+        self.ack = ack
+        self.syn = syn
+        self.fin = fin
+	
 		
-			
-		
-    class Sender:
-        
-        def __init__(self,  rHostIp, recPort, file, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed):
-            self.rHostIp = rHostIp
-            self.recPort = int(recPort)
-            self.file = file
-            self.MWS = int(MWS)
-            self.MSS = int(MSS)
-            self.gamma = float(gamma)
-            self.pDrop = pDrop
-            self.pDup = pDup
-            self.pCor = pCor
-            self.pOrd = pOrd
-            self.maxOrd = maxOrd
-            self.pDel = pDel
-            self.maxDel = maxDel
-            self.seed = int(seed)
-            
-            
-		
-	    #create a UDP socket for the sender
 	
-	    socket = socket(AF_INET, SOCK_DGRAM) 
+class Sender:
+    
+    def __init__(self,  rHostIp, recPort, sFile, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed):
+        self.rHostIp = rHostIp
+        self.recPort = int(recPort)
+        self.sFile = sFile
+        self.MWS = int(MWS)
+        self.MSS = int(MSS)
+        self.gamma = float(gamma)
+        self.pDrop = pDrop
+        self.pDup = pDup
+        self.pCor = pCor
+        self.pOrd = pOrd
+        self.maxOrd = maxOrd
+        self.pDel = pDel
+        self.maxDel = maxDel
+        self.seed = int(seed)
+        
+        
 	
-	    #store file as data to send to receiver 
-	
-	    def pullFile(self):
-	        f = open(self.file, "r")
-	        data = f.read()
-	        return data
-	        
-        #def createAck(self, seqNum, ackNum):
+    #create a UDP socket for the sender
+
+    socket = socket(AF_INET, SOCK_DGRAM) 
+
+    #store file as data to send to receiver 
+
+    def pullFile(self):
+        f = open(self.sFile, "r")
+        data = f.read()
+        f.close()
+        return data
+        
+    #def createAck(self, seqNum, ackNum):
+        
+    
+    
+    
+    #def createFIN(self, seqNum, ackNum):
+    
+    #def createSYN(self, seqNum, ackNum):
+    
+    def receivePacket(self):
+        data, addr = self.socket.recvfrom(2048)
+        stpPacket = pickle.loads(data)
+        return stpPacket
+    
+    
+    #create packet of max seg size
+    def splitDataToLoad(self, completeData, dataProgress):
+        dataLength = len(completeData)
+        end = dataProgress + self.MSS
+        if (end < dataLength):
+            load = completeData[dataProgress:end]
+        
+        else: 
+            load = completeData[dataProgress:dataLength]
+        return load
+    
+    
+    def sendPacket(self, packet):
+        self.socket.sendto(pickle.dumps(packet), (self.rHostIp, self.recPort))
+    
+    
+    
+    #def logU(self, , , ):
+
+
+#### MAIN 
+
+if (len(sys.argv) != 15):
+    print("incorect usage, 14 arguments needed")
+    exit(0)
+
+seqNum = 0
+ackNum = 0
+#sendbase = 0
+#numUnAck = 0
+
+# different states of operation
+noConnection = True
+synSent = False
+connected = False
+finSent = False
+connectionFinished = False
+
+transCount = 0
+untranCount = 0
+dropCount = 0
+
+rHostIp, recPort, sFile, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed = sys.argv[1:]
+
+###sener is initiated
+
+sender = Sender(rHostIp, recPort, sFile, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed)
+sendFile = sender.pullFile()
+fileLength = len(sendFile)
+dataSent = 0
+
+while (connectionFinished == False):
+    
+    
+    ###send syn
+    if (noConnection == True):
+        print("sending syn")
+        synPacket = Packet('', seqNum, ackNum, ack = False, syn = True, fin = False)
+        #print(synPacket.seqNum)
+        sender.sendPacket(synPacket)
+        synSent = True
+        noConnection = False
+        
+    if (synSent == True):
+        print("waiting for synAck")
+        synAckPacket = sender.receivePacket()
+        
+        if (synAckPacket.ack == True and synAckPacket.syn == True):
+            
+            ackNum = synAckPacket.seqNum + 1
+            seqNum = seqNum + 1
+            
+            ackPacket = Packet('', seqNum, ackNum, ack = True, syn = False, fin = False)
+            sender.sendPacket(ackPacket)
+            synSent = False
+            connected = True
+            print("sent ack, starting file transfer")
+        else:
+            print("error in synSent flag stage")
             
         
+    ###main bit of while loop for open connection
+    if (connected == True):
+    
+        
+        ###create the packet to be sent
+        load = sender.splitDataToLoad(sendFile, dataSent)
+        dataSent += len(load)
+        ## maybe need to increment sequence number here?
+        packet = Packet(load, seqNum, ackNum, ack = False, syn = False, fin = False)
+        
+        ## this is where pld module will go ventually
+        
+        #send packet
+        print("sent packets")
+        print(len(packet.data), packet.seqNum)
+        sender.sendPacket(packet)
+        seqNum += len(load)
+        transCount = transCount + 1
         
         
-        #def createFIN(self, seqNum, ackNum):
+        ##receive same packet
+        ackPacket = sender.receivePacket()
+        ackNum = ackPacket.seqNum + 1
         
-        #def createSYN(self, seqNum, ackNum):
+        if (ackPacket.ack == True and ackPacket.ackNum == seqNum):
+            print("correct acknowledgment received")
+            ##do what you need to do when an ack is received from the receiver
         
-        def receivePacket(self):
-            data, addr = self.socket.recvfrom(2048)
-            stpPacket = pickle.loads(data)
-            return stpPacket
-        
-        
-        #create packet of max seg size
-        def splitDataToLoad(self, completeData, dataProgress):
-            dataLength = len(completeData)
-            end = completeData + self.MSS
-            if (end < dataLength):
-                load = completeData[dataProgress:dataLength]
+        if (dataSent == fileLength):
+            print("everything received, file ready, fin sent")
+            finSent = True
+            connected = False
+            finPacket = Packet('', seqNum, ackNum, ack = False, syn = False, fin = True)
+            sender.sendPacket(finPacket)
+
+    if (finSent == True):
+        ackPacket = sender.receivePacket()
+        if (ackPacket.ack == True and ackPacket.fin == False):
+            print("fin acknowledgment received")
+            finPacket = sender.receivePacket() 
+            ackNum = ackNum + 1
             
-            else: 
-                load = completeData[dataProgress:end]
-            return load
-        
-        
-        def sendPacket(self, packet):
-            self.socket.sendto(pickle.dumps(packet), (self.rHostIp, self.recPort))
-        
-        
-        
-        def logU(self, , , ):
-    
-    
-    #### MAIN 
-	
-    if (len(sys.argv) != 15):
-        print("incorect usage")
-        return
-    
-    seqNum = 0
-    ackNum = 0
-    #sendbase = 0
-    #numUnAck = 0
-    
-    # different states of operation
-    noConnection = True
-    synSent = False
-    connected = False
-    finSent = False
-    connectionFinished = False
-    
-    transCount = 0
-    untranCount = 0
-    dropCount = 0
-    
-    rHostIp, recPort, file, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed = sys.argv[1:]
-    
-    ###sener is initiated
-    
-    sender = Sender(rHostIp, recPort, file, MWS, MSS, gamma,pDrop, pDup, pCor, pOrd, maxOrd, pDel, maxDel, seed)
-    sendFile = sender.pullFile()
-    fileLength = len(sendFile)
-    dataSent = 0
-    
-    while (connectionFinished == False):
-        
-        
-        ###send syn
-        if noConnection == True:
-            synPacket = Packet('', seqNum, ackNum, ack = False, syn = True, fin = False )
-            sender.sendPacket(senPacket)
-            synSent = True
-            noConnection = False
-            
-        if synSent = True
-            synAckPacket = sender.receivePacket()
-            
-            if synAckPacket.ack == True && synAckPacket.syn == True:
-                
-                ackNum = synAckPacket.seqNum + 1
-                seqNum = seqNum + 1
-                
-                ackPacket = packet('', seqNum, ackNum, ack = True, syn = False, fin = False)
+            if (finPacket.fin == True):
+                finSent = False
+                ackPacket = Packet('', ackNum, seqNum, ack = True, syn = False, fin = False)
                 sender.sendPacket(ackPacket)
-                synSent = False
-                connected = True
-            else:
-                print("error in synSent flag stage")
-                
-            
-        ###main bit of while loop for open connection
-        if connected = True:
-        
-            
-            ###create the packet to be sent
-            load = sender.splitDataToLoad(sendFile, dataSent)
-            dataSent += len(load)
-            ## maybe need to increment sequence number here?
-            packet = Packet(load, seqNum, ackNum, ack = False, syn = False, fin = False)
-            
-            ## this is where pld module will go ventually
-            
-            #send packet
-            sender.sendPacket(packet)
-            seqNumber += len(load)
-            transCount++
-            
-            
-            ##receive same packet
-            ackPacket = sender.receivePacket()
-            ackNum = ackPacket.seqNum + 1
-            
-            if ackPacket.ack == True and ackPacket.ackNum == seqNum:
-                
-                ##do what you need to do when an ack is received from the receiver
-            
-            if dataSent == fileLength:
-                print("everything received, file ready, start fin")
-                finSent = True
-                connected = False
-                finPacket = Packet('', seqNum, ackNum, ack = False, syn = False, fin = True)
-                sender.sendPacket(finPacket)
+                print("connection terminated and fin completed")
+                connectionFinished = True
     
-        if finSent = True:
-            ackPacket = sender.receivePacket()
-            if ackPacket.ack == True and ackPacket.fin = False:
-                finPacket = sender.receivePacket() 
-                ackNum++
-                
-                if finPacket.fin == True:
-                    
-                    ackPacket = Packet('', ackNum, seqNum, ack = True, syn = False, Fin = False)
-                    sender.sendPacket(ackPacket)
-                    print("connection terminated and fin completed")
-        
-        
-        sender.socket.close()
-        
-                    
+sender.socket.close()
+    
                 
             
-            
         
+        
+    
         
     	
 		
